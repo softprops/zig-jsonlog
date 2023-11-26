c1a6b147e84bb800cc8d744d157a34e7a3c7bca6
softprops <d.tangren@gmail.com>
2023-11-26 12:42:51 -0500

    clean

--- src/main.zig
+++ src/main.zig
@@ -44,20 +44,10 @@ pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    //const stdout_file = std.io.getStdOut().writer();
-    //var bw = std.io.bufferedWriter(stdout_file);
-    //const stdout = bw.writer();
-
-    //try stdout.print("Run `zig build test` to run the tests.\n", .{});
     std.log.debug("test 1", .{});
     std.log.info("test 2", .{});
     std.log.warn("test 3", .{});
     std.log.err("test 4", .{});
-
-    //try bw.flush(); // don't forget to flush!
 }
 
 test "simple test" {
